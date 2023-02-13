216c098bccf34cb35de9c3b31b6b6c562bc872a0
xiaoliuxiao6 <46404150+xiaoliuxiao6@users.noreply.github.com>
2023-02-13 17:25:13 +0800

    Update centos7_unconfig.sh

--- centos7_unconfig.sh
+++ centos7_unconfig.sh
@@ -2,6 +2,7 @@
 set -o errexit 
 set -o nounset
 
+
 ## 删除 ssh 主机密钥
 rm -rf /etc/ssh/ssh_host_*
 
@@ -9,6 +10,12 @@ rm -rf /etc/ssh/ssh_host_*
 [[ `grep -l HWADDR /etc/sysconfig/network-scripts/ifcfg-* | wc -l` -gt 0 ]] && sed -i "/HWADDR/d" `grep -l HWADDR /etc/sysconfig/network-scripts/ifcfg-*`
 [[ `grep -l UUID /etc/sysconfig/network-scripts/ifcfg-* | wc -l` -gt 0 ]] && sed -i "/UUID/d" `grep -l UUID /etc/sysconfig/network-scripts/ifcfg-*`
 
-
 ## 删除日志
 rm -rf /var/log/*
+
+## 执行成功
+echoCyan(){
+    echo -e "\033[36m$*\033[0m"
+}
+
+echoCyan "Success"
